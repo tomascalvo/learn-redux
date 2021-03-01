0ff7862f11f9b7ae36956b366f1c0e2832061101
Tomás Calvo <tomasrcalvo@gmail.com>
2021-03-01 10:47:25 -0500

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const increment = nr => {
+  return {
+    type: 'INCREMENT',
+    payload: nr
+  }
+}
+export const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+export const signIn = () => {
+  return {
+    type: 'SIGN_IN'
+  }
+}
